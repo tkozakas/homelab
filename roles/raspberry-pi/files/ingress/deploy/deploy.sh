a9b366c24953cd5c3f51e4091694768f7c297fca
jarvis <jarvis@localhost>
2026-09-07 23:43:35 +0000

    Remove orphan containers when starting stacks

--- roles/raspberry-pi/files/ingress/deploy/deploy.sh
+++ roles/raspberry-pi/files/ingress/deploy/deploy.sh
@@ -3,6 +3,6 @@ set -e
 
 echo "Deploying $(pwd)"
 docker compose --profile prod pull
-docker compose --profile prod up -d
+docker compose --profile prod up -d --remove-orphans
 docker image prune -f
 echo "Deploy complete"
