9ffe0357bf09dcb37939cd912e0706c8be31c39d
Tomas Kozakas <tomas.kozakas@vinted.com>
2026-09-08 14:53:54 +0300

    Restore deploy lock and got health gating

--- roles/raspberry-pi/files/ingress/deploy/deploy.sh
+++ roles/raspberry-pi/files/ingress/deploy/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+exec 9>"/tmp/deploy-$(basename "$(pwd)").lock"
+flock 9
+
 echo "Deploying $(pwd)"
 docker compose --profile prod pull
 docker compose --profile prod up -d --remove-orphans
