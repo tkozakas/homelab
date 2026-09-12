3b74f1241fb4c14d1da0c7b418caeebea6f313ce
jarvis <jarvis@localhost>
2026-09-12 13:02:41 +0000

    Give jarvis explicit DNS servers and ingress network access

--- roles/raspberry-pi/files/ingress/deploy/deploy.sh
+++ roles/raspberry-pi/files/ingress/deploy/deploy.sh
@@ -5,6 +5,7 @@ exec 9>"/tmp/deploy-$(basename "$(pwd)").lock"
 flock 9
 
 echo "Deploying $(pwd)"
+docker network inspect ingress >/dev/null 2>&1 || docker network create ingress
 docker compose --profile prod pull
 docker compose --profile prod up -d --remove-orphans
 docker image prune -f
